3f8443e23a43d5b971a66f468d2b3741bc981e2b
theIncredibleMarek <marek.furak+github@gmail.com>
2016-11-16 15:31:44 +0100

    adding missing info in headers

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from suds.client import Client
 
 def getCreditScore():
--- rmqConsume.py
+++ rmqConsume.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import pika
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
