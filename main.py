098810e5702b2ac0c30ce8322f6c48f07cbc4517
theIncredibleMarek <marek.furak+github@gmail.com>
2016-11-16 15:29:13 +0100

    refactor main.py

--- main.py
+++ main.py
@@ -4,11 +4,8 @@ def getCreditScore():
 	client = Client('http://139.59.154.97:8080/CreditScoreService/CreditScoreService?wsdl')
 	# no idea why this doesn't work - Teo
 	# Marek fixed it
-	# request_data = client.factory.create('tns:creditScore')
-	# request_data.ssn = '123456-1234'
 	result = client.service.creditScore('123456-1234')
-	print "type of ",type(result)
-	return result
-
+	# print "type of ",type(result)
+	return "Anser from API: " + str(result)
 
 print getCreditScore()
