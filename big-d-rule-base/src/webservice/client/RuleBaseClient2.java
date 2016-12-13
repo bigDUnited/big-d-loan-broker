966b95bdc23557186ab33fb873fa38fb8dcc3ce0
bobkoobg <boyko.surlev@gmail.com>
2016-12-13 19:03:07 +0100

    Gitignore wsimport folder

--- big-d-rule-base/src/webservice/client/RuleBaseClient2.java
+++ big-d-rule-base/src/webservice/client/RuleBaseClient2.java
@@ -1,17 +1,17 @@
-//package webservice.client;
-//
-//import webservice.soap.RuleBaseInterface;
-//import webservice.soap.RuleBaseImplementationService;
-//
-//public class RuleBaseClient2 {
-//
-//    public static void main(String[] args) {
-//
-//        RuleBaseImplementationService helloService = new RuleBaseImplementationService();
-//        RuleBaseInterface rbi = (RuleBaseInterface) helloService.getRuleBaseImplementationPort();
-//
-//        System.out.println("Remote Request 1: " + rbi.getBanksByCreditScoreJson(690));
-//        System.out.println("Remote Request 2: " + rbi.getBanksByCreditScoreJson(550));
-//    }
-//
-//}
+package webservice.client;
+
+import webservice.soap.RuleBaseInterface;
+import webservice.soap.RuleBaseImplementationService;
+
+public class RuleBaseClient2 {
+
+    public static void main(String[] args) {
+
+        RuleBaseImplementationService helloService = new RuleBaseImplementationService();
+        RuleBaseInterface rbi = (RuleBaseInterface) helloService.getRuleBaseImplementationPort();
+
+        System.out.println("Remote Request 1: " + rbi.getBanksByCreditScoreJson(690));
+        System.out.println("Remote Request 2: " + rbi.getBanksByCreditScoreJson(550));
+    }
+
+}
